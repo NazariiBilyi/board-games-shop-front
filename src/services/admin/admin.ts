5d7b0a05d5f167dcf29fbedf9611c2511956af22
Nazarii Bilyi <nazar.biliy@abcfintess.com>
2025-05-30 13:59:09 +0300

    Changed the routes for the admin panel

--- src/services/admin/admin.ts
+++ src/services/admin/admin.ts
@@ -8,15 +8,15 @@ import {IGenericResponse} from "../types.ts";
 
 export const AdminService = {
     async createNewItem (itemType: string, data: IItemPayload): Promise<AxiosResponse<ICreateNewItemResponse>> {
-        return await http.put(`/admin/add-new-item/${itemType}`, data)
+        return await http.put(`/admin/item/${itemType}`, data)
     },
 
     async uploadTitleImage (formData: FormData): Promise<AxiosResponse<IUploadTitleImageResponse>> {
-        return await http.put('/admin/upload-title-image', formData)
+        return await http.put('/admin/item/image', formData)
     },
 
     async uploadItemImages(formData: FormData): Promise<AxiosResponse<IUploadImagesResponse>> {
-        return await http.put(`/admin/upload-images/`, formData, {
+        return await http.put(`/admin/item/images`, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             }
@@ -24,10 +24,10 @@ export const AdminService = {
     },
 
     async getItemsByType(type: string): Promise<AxiosResponse<IBoardGamesResponse>> {
-        return await http.get(`/admin/get-items/${type}`)
+        return await http.get(`/admin/items/${type}`)
     },
 
     async deleteItemByType(type: string, itemId: string): Promise<AxiosResponse<IGenericResponse>> {
-        return await http.delete(`/admin/delete-item/${type}/${itemId}`)
+        return await http.delete(`/admin/item/${type}/${itemId}`)
     }
 }
